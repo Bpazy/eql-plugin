3e5970622946eb766d468fdfcf14fbf2d6754ab8
ziyuan <hanziyuan08@outlook.com>
2017-11-01 09:49:42 +0800

    jump to the corresponding eql file

--- src/main/java/com/github/bpazy/JumpToEqlIntention.java
+++ src/main/java/com/github/bpazy/JumpToEqlIntention.java
@@ -2,8 +2,10 @@ package com.github.bpazy;
 
 import com.intellij.codeInsight.intention.impl.BaseIntentionAction;
 import com.intellij.openapi.editor.CaretModel;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.LogicalPosition;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
@@ -25,6 +27,8 @@ import java.io.StringReader;
  * created on 2017/10/28
  */
 public class JumpToEqlIntention extends BaseIntentionAction {
+
+    private final int NOT_EXIST_EQL_METHOD = -1;
 
     @NotNull
     @Override
@@ -58,13 +62,6 @@ public class JumpToEqlIntention extends BaseIntentionAction {
         return text.contains("new Eql") || text.contains("new Dql");
     }
 
-    private PsiElement extraEqlStatement(PsiElement element) {
-        if (element == null || element instanceof PsiExpressionStatement) {
-            return element;
-        }
-        return extraEqlStatement(element.getParent());
-    }
-
     @Override
     public void invoke(@NotNull Project project, Editor editor, PsiFile psiFile) throws IncorrectOperationException {
         int offset = editor.getCaretModel().getOffset();
@@ -77,33 +74,55 @@ public class JumpToEqlIntention extends BaseIntentionAction {
 
         PsiFile[] files = PsiShortNamesCache.getInstance(project).getFilesByName(eqlFileName);
         for (PsiFile file : files) {
+            int lineNum = existEqlMethod(file, methodName);
+            if (lineNum == NOT_EXIST_EQL_METHOD) {
+                continue;
+            }
+
+            // 打开对应eql文件
             OpenFileDescriptor descriptor = new OpenFileDescriptor(project, file.getVirtualFile());
             Editor eqlEditor = FileEditorManager.getInstance(project).openTextEditor(descriptor, true);
             if (eqlEditor == null) return;
-
-            BufferedReader reader = new BufferedReader(new StringReader(eqlEditor.getDocument().getText()));
-            try {
-                int lineNum = 0;
-                String s;
-                while ((s = reader.readLine()) != null) {
-                    if (s.matches("--\\s*\\[" + methodName + "]")) break;
-                    lineNum++;
-                }
-
-                // s 为空则eql文件不含有对应函数
-                if (s == null) {
-                    break;
-                }
-
-                // 跳转到eql文件中函数的位置
-                CaretModel caretModel = eqlEditor.getCaretModel();
-                LogicalPosition logicalPosition = caretModel.getLogicalPosition();
-                logicalPosition.leanForward(true);
-                LogicalPosition logical = new LogicalPosition(lineNum, logicalPosition.column);
-                caretModel.moveToLogicalPosition(logical);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            // 跳转到eql文件中函数的位置
+            CaretModel caretModel = eqlEditor.getCaretModel();
+            LogicalPosition logicalPosition = caretModel.getLogicalPosition();
+            logicalPosition.leanForward(true);
+            LogicalPosition logical = new LogicalPosition(lineNum, logicalPosition.column);
+            caretModel.moveToLogicalPosition(logical);
         }
+    }
+
+    private PsiElement extraEqlStatement(PsiElement element) {
+        if (element == null || element instanceof PsiExpressionStatement) {
+            return element;
+        }
+        return extraEqlStatement(element.getParent());
+    }
+
+    /**
+     * Eql文件中对应函数行号
+     * @param file eql文件
+     * @param methodName 函数名称
+     * @return 函数所在行号，-1则不存在
+     */
+    private int existEqlMethod(PsiFile file, String methodName) {
+        Document document = FileDocumentManager.getInstance().getCachedDocument(file.getVirtualFile());
+        if (document == null) {
+            return NOT_EXIST_EQL_METHOD;
+        }
+        String text = document.getText();
+        BufferedReader reader = new BufferedReader(new StringReader(text));
+
+        try {
+            int lineNum = 0;
+            String s;
+            while ((s = reader.readLine()) != null) {
+                if (s.matches("--\\s*\\[" + methodName + "]")) return lineNum;
+                lineNum++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return NOT_EXIST_EQL_METHOD;
     }
 }
